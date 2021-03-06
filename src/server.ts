e72560a5ca775122212cbbaef76b039cb99ec385
nurik <nurik6@gmail.com>
2021-03-06 05:30:19 +0500

    feat: remove serving static files

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,11 @@
 import { run } from './app';
 import { render } from './render';
 import { log } from './logger';
-import { promises as fs } from 'fs';
 import { ServerResponse } from 'http';
-import { join } from 'path';
 import { Cache } from './Cache';
 const cache = new Cache();
 
-module.exports = async (req: Request, res: ServerResponse) => {
-	if (isStaticFile(req.url)) {
-		try {
-			const data = await openFile(req.url, cache);
-			res.end(data);
-			return;
-		} catch (err) {
-			res.statusCode = 404;
-			res.end('Page not found!');
-			return;
-		}
-	}
+module.exports = async (_: Request, res: ServerResponse) => {
 	const data = cache.get('page');
 	if (data) {
 		res.end(data);
@@ -29,14 +16,6 @@ module.exports = async (req: Request, res: ServerResponse) => {
 		res.end(outputHtml);
 	}
 };
-
-function isStaticFile(url: string) {
-	return /\/tailwind\.css/i.test(url) || /\/favicon\.ico/i.test(url);
-}
-
-async function openFile(url: string, cache: Cache): Promise<Buffer> {
-	return await cache.getset(url, async () => fs.readFile(join(__dirname, '../public', url)), 999999999999);
-}
 
 process.on('SIGINT', function () {
 	log.info('ctrl+c');
