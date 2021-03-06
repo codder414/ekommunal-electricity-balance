98dfb67fcf59e9c92a294c1507f215175e8aa9ab
nurik <nurik6@gmail.com>
2021-03-06 07:12:36 +0500

    feat: add base url to template

--- src/render.ts
+++ src/render.ts
@@ -1,10 +1,12 @@
 import * as pug from 'pug';
 import * as path from 'path';
+import { config } from './config';
 
 export async function render(data: string[][]): Promise<string> {
 	const rendered = pug.renderFile(path.join(__dirname, '../layout/template.pug'), {
 		header: data[0],
-		rows: data.slice(1)
+		rows: data.slice(1),
+		baseUrl: config.BASE_URL
 	});
 	return rendered;
 }
