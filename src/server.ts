c56b6e30ca772fe0b74ec0e0f2828805f1e57eb6
nurik <nurik6@gmail.com>
2021-03-06 08:07:18 +0500

    feat: replace micro with express

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import { run } from './app';
 import { render } from './render';
 import { log } from './logger';
-import { ServerResponse } from 'http';
 import { Cache } from './Cache';
 import { config } from './config';
+import * as Express from 'express';
+import * as path from 'path';
+
 const cache = new Cache();
 
-module.exports = async (req: Request, res: ServerResponse) => {
-	if (req.url.replace(/\/$/, '') !== config.BASE_URL.replace(/\/$/, '')) {
-		res.statusCode = 404;
-		res.end('<h1>Page not found!</h1>');
-		return;
-	}
+const app = Express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../layout'));
+
+if (config.NODE_ENV === 'development') {
+	app.use(Express.static(path.join(__dirname, '../', './public')));
+}
+
+app.get('/', async (_, res) => {
 	const data = cache.get('page');
 	if (data) {
 		res.end(data);
@@ -21,8 +27,28 @@ module.exports = async (req: Request, res: ServerResponse) => {
 		cache.set('page', outputHtml, 60000);
 		res.end(outputHtml);
 	}
-};
+});
 
+app.use(function (req, res) {
+	res.status(404);
+
+	// respond with html page
+	if (req.accepts('html')) {
+		res.render('404', { url: req.url });
+		return;
+	}
+
+	// respond with json
+	if (req.accepts('json')) {
+		res.send({ error: 'Not found' });
+		return;
+	}
+
+	// default to plain-text. send()
+	res.type('txt').send('Not found');
+});
+
+app.listen(3000, '0.0.0.0', () => log.info('Server listening on 0.0.0.0:3000...'));
 process.on('SIGINT', function () {
 	log.info('ctrl+c');
 	cache.clear();
